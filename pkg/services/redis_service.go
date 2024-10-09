f63730f8d36851f3b6bb27148f0c2790919498ac
Anas Khan <anas.k@ecobee.com>
2024-10-08 23:53:29 -0400

    chang redis client to continer name

--- pkg/services/redis_service.go
+++ pkg/services/redis_service.go
@@ -13,9 +13,9 @@ var ctx = context.Background()
 // Initialize Redis connection
 func InitRedis() {
 	redisClient = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379", // Redis address
-		Password: "",               // No password set
-		DB:       0,                // Use default DB
+		Addr:     "redis:6379", // Redis address
+		Password: "",           // No password set
+		DB:       0,            // Use default DB
 	})
 
 	// Test the connection
